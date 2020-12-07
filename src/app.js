910af01cb986328eb9d7b289096935dcb0a40051
shamsh000 <46910881+shamsh000@users.noreply.github.com>
2020-12-07 17:00:47 +0530

    setup changes for heroku

--- src/app.js
+++ src/app.js
@@ -5,6 +5,7 @@ const forecast = require('./utils/forecast')
 const geocode = require('./utils/geocode')
 
 const app = express()
+const port = process.env.PORT || 3000
 const publicDirectoryPath = path.join(__dirname, '../public')
 const viewsPath = path.join(__dirname, '../templates/views')
 const partialsPath = path.join(__dirname, '../templates/partials')
@@ -88,6 +89,6 @@ app.get('*', (req, res) => {
     })
 })
 
-app.listen(3000, ()=>{
-    console.log('web server is up !')
+app.listen(port, ()=>{
+    console.log('web server is up at ' + port)
 })
